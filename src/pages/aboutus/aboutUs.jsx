b2d37a8164eb74543ce7c9cb08e611fe047d3ec1
Aathavan Thevakumar <aathavanthevakumar@gmail.com>
2026-01-05 11:40:22 +0100

    fix vercel prob

--- src/pages/aboutus/aboutUs.jsx
+++ src/pages/aboutus/aboutUs.jsx
@@ -11,8 +11,7 @@ import reactjsvitrine from "../../assets/images/home/reactjsvitrine.png";
 import springbootvitrine from "../../assets/images/home/springbootvitrine.png";
 import workflowData from "../../assets/json/workflow.json";
 import AboutUsHero from "../../components/aboutUs/AboutUsHero.jsx";
-import MaScene3D from "../../assets/3D/Exportsalledecours.glb";
-import '@google/model-viewer';
+import "@google/model-viewer";
 
 export default function AboutUs() {
   const [animationData, setAnimationData] = useState(null);
@@ -45,19 +44,28 @@ export default function AboutUs() {
         <div className="grid grid-cols-1 md:grid-cols-12 gap-6 px-5">
           <div className="md:col-span-7 p-6">
             <p className="mt-0 md:mt-8 text-justify text-lg">
-              Découvrez un environnement conçu pour la réussite. Nos salles de classe ne sont pas de simples pièces,
-              mais des écosystèmes technologiques pensés pour favoriser l'interaction, la concentration et la créativité.
-              Grâce à notre modélisation 3D, explorez chaque détail de l'équipement mis à votre disposition.
+              Découvrez un environnement conçu pour la réussite. Nos salles de
+              classe ne sont pas de simples pièces, mais des écosystèmes
+              technologiques pensés pour favoriser l'interaction, la
+              concentration et la créativité. Grâce à notre modélisation 3D,
+              explorez chaque détail de l'équipement mis à votre disposition.
             </p>
             <p className="mt-7 text-justify text-lg">
-              Au-delà de l'esthétique, notre approche repose sur la synergie entre l'humain et son espace de travail.
-              Nous croyons que la qualité des finitions — du grain satiné du mobilier à la clarté du système acoustique —
-              influence directement la capacité d'assimilation et le bien-être des apprenants. Chaque élément modélisé
-              ici reflète notre engagement : offrir un cadre professionnel, épuré et hautement technologique.
+              Au-delà de l'esthétique, notre approche repose sur la synergie
+              entre l'humain et son espace de travail. Nous croyons que la
+              qualité des finitions — du grain satiné du mobilier à la clarté du
+              système acoustique — influence directement la capacité
+              d'assimilation et le bien-être des apprenants. Chaque élément
+              modélisé ici reflète notre engagement : offrir un cadre
+              professionnel, épuré et hautement technologique.
             </p>
           </div>
           <div className="md:col-span-5 pd-3 md:p-6">
-            <img src={quisommesnous} alt="Aperçu salle" className="h-auto mx-auto rounded-xl shadow-md" />
+            <img
+              src={quisommesnous}
+              alt="Aperçu salle"
+              className="h-auto mx-auto rounded-xl shadow-md"
+            />
           </div>
         </div>
       </div>
@@ -69,17 +77,14 @@ export default function AboutUs() {
           ar
           camera-controls
           touch-action="pan-y"
-
           /* POSITION DE LA CAMÉRA */
-          camera-orbit="0deg 75deg 2m"  /* Angle et distance (2m pour être au centre) */
-          field-of-view="75deg"         /* Grand angle pour l'immersion */
+          camera-orbit="0deg 75deg 2m" /* Angle et distance (2m pour être au centre) */
+          field-of-view="75deg" /* Grand angle pour l'immersion */
           min-camera-orbit="auto auto 0m" /* Permet de zoomer jusqu'à 0m (intérieur) */
-
           /* ÉCLAIRAGE (pour éviter l'effet aveuglant) */
           environment-intensity="0.7"
           exposure="0"
-
-          style={{ width: '100%', height: '600px', backgroundColor: '#2c3e50' }}
+          style={{ width: "100%", height: "600px", backgroundColor: "#2c3e50" }}
         >
           {/* Optionnel : Masquer les faces arrière des murs pour voir à travers quand on est dehors */}
         </model-viewer>
